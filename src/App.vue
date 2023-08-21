59e898f8a4e133e67d5a825ae7ca2d78d21b2e2d
lexmin0412 <zhangle_dev@outlook.com>
2023-08-21 13:12:27 +0800

    feat: 引入 ant-design-vue

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
+		<a-button type="primary">Primary Button</a-button>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
